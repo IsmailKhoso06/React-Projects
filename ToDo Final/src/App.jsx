903f5003974e2c179fe9d847a1cf11dbec7777ca
Ismail <mismailkhoso006@gmailcom>
2024-05-10 19:11:42 +0500

    Developed New TO-DO App

---
+++ ToDo Final/src/App.jsx
@@ -0,0 +1,37 @@
+import AddToDo from "./components/AddToDo";
+import AppName from "./components/AppName";
+import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./components/WelcomeMessage";
+import { useState } from "react";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddToDo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
+    </center>
+  );
+}
+
+export default App;
